a5bc70bbe3a32b572e743db9f4fb88924c2951b0
Anil Tarar <aniltarar@gmail.com>
2021-08-03 15:45:16 +1000

    Added the rule for winning condition.

--- static/script.js
+++ static/script.js
@@ -1,8 +1,27 @@
 const xClass = "X";
 const oClass = "O";
+const winningRules = [
+    [0 , 1 , 2],
+    [3 , 4 , 5],
+    [6 , 7 , 8],
+    [0 , 3 , 6],
+    [1 , 4 , 7],
+    [2 , 5 , 8],
+    [0 , 4 , 8],
+    [2 , 4 , 6]
+]
+
 const cells = document.querySelectorAll('[data-cell]');
 
 let circleTurn = true;
+
+function Rules(currentPlayer){
+    return winningRules.some(combination => {
+        return combination.every(index => {
+            return cells[index].textContent === currentPlayer;
+        })
+    })
+} 
 
 function swapSides() {
     circleTurn = !circleTurn;
@@ -14,6 +33,13 @@ function clickManager(event){
     const currentMark = circleTurn ? oClass : xClass;
     cell.textContent = currentMark;
     console.log(currentMark);
+    if (Rules(currentMark)){
+        if(currentMark === "O"){
+            alert("First Player WINS!!!");
+        }else {
+            alert("Second Player WINS!!!")
+        }
+    };
     swapSides();
 };
 
